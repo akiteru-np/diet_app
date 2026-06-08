ba26244f0e80ecbbefb7caed22d91e17418d6f66
teru <hotahotateru223@gmail.com>
2026-06-09 04:52:23 +0900

    Render無料枠のIPv6非対応問題を回避するため、SupabaseのPooler(IPv4)接続URLへ変更！

--- database.py
+++ database.py
@@ -2,10 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# 🔽 記号を安全な形（%23, %26）に変換し、最後の文字も修正した完璧なURLです！
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:e3fcv7LXK%2336m%26P@db.cqzjilubjxixdrkooqjr.supabase.co:5432/postgres"
+# 🔽 Renderの無料枠でも確実につながる「Pooler（IPv4）仕様」の完璧なURLです！
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres.cqzjilubjxixdrkooqjr:e3fcv7LXK%2336m%26P@aws-0-ap-northeast-1.pooler.supabase.com:6543/postgres"
 
-# PostgreSQLの場合はシンプルな設定でOKです！
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
