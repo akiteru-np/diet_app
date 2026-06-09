433f738571a69bc0645775b740ba3b33ea910e94
teru <hotahotateru223@gmail.com>
2026-06-10 00:33:54 +0900

    SQLAlchemy用のSSL強制とSessionポートを指定し、Supabaseと完全接続！

--- database.py
+++ database.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# 🔽 Renderの無料枠でも確実につながる「Pooler（IPv4）仕様」の完璧なURLです！
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres.cqzjilubjxixdrkooqjr:e3fcv7LXK%2336m%26P@aws-0-ap-northeast-1.pooler.supabase.com:6543/postgres"
+# 🔽 【最終奥義】ドライバー指定(psycopg2)、Sessionポート(5432)、SSL強制(sslmode=require)をすべて盛り込んだ完全版！
+SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres.cqzjilubjxixdrkooqjr:e3fcv7LXK%2336m%26P@aws-0-ap-northeast-1.pooler.supabase.com:5432/postgres?sslmode=require"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
